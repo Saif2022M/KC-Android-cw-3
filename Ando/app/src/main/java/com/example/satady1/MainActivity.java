5a04ebed7cde8d67885ac78924d4a18e2b1bea52
Saif2022M <saifaldin206@gmail.com>
2022-08-05 19:29:19 +0300

    Done

---
+++ Ando/app/src/main/java/com/example/satady1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.satady1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+       EditText First  = findViewById(R.id.First);
+       EditText Second = findViewById(R.id.Secand);
+
+
+
+
+
+    }
+}
